2cc753c18d3404ed2411f148329419bad2cf7b78
yuyi5453 <744530689@qq.com>
2020-12-09 20:45:53 +0800

    vue

--- src/main/java/controller/JobInfoController.java
+++ src/main/java/controller/JobInfoController.java
@@ -24,7 +24,6 @@ import javax.servlet.http.HttpSession;
  * @since 2020-12-06
  */
 @Controller
-//@RequestMapping("/jobInfo")
 public class JobInfoController {
     @Autowired
     private JobInfoService jobInfoService;
@@ -48,7 +47,7 @@ public class JobInfoController {
         return new RestResult().setCode(ResultCode.FAIL).setMessage("职位已存在").toString();
     }
 
-    @RequestMapping("/addJob")
+    @RequestMapping("/removeJob")
     @ResponseBody
     public String removeJob(HttpServletRequest request){
         String jobId=request.getParameter("jobId");
