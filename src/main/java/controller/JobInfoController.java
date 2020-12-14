31643377d7c07d83789322b1000a0d5b12dd9f57
yuyi5453 <744530689@qq.com>
2020-12-14 15:39:09 +0800

    jobpage

--- src/main/java/controller/JobInfoController.java
+++ src/main/java/controller/JobInfoController.java
@@ -33,8 +33,9 @@ public class JobInfoController {
     public String addJob(HttpSession session, HttpServletRequest request){
         JobInfo job=new JobInfo();
         String companyId=(String)session.getAttribute("user");
+//        String companyId="123";
         job.setCompanyId(companyId);
-        String jobName=(String)request.getParameter("companyId");
+        String jobName=(String)request.getParameter("jobName");
         job.setJobName(jobName);
         String jobType=(String)request.getParameter("jobType");
         job.setJobType(jobType);
